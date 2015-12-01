dd8bcdf70d83cf0513f35badf3c439d58092d549
gmaurer <Gabe@Gabes-MacBook-Pro.local>
2015-11-30 23:09:06 -0600

    Added spelling check for ie ei and ible and able

--- buildAWord.py
+++ buildAWord.py
@@ -9,19 +9,78 @@
 #search for those.
 import csv
 import sys
+
 from random import randint
 
 
-def buildIt(alphabet,dictionary):
+def buildIt(vowel,consonant,dictionary):
 	newWord = ''
-	random = randint(3,5)
+	random = randint(3,9)
+	alphabet = []
+	alphabet = vowel + consonant
+	dicLength = len(dictionary)-1
+	ible = "ible"
+	able = "able"
+	ableLess = 0
+	ibleLess = 0
 	
 	for i in range(random):
-		dicRandom = randint(0,(len(alphabet)-1))
-		newWord+= alphabet[dicRandom]
+
+		vOrC = randint(0,3)
+		if vOrC%3 == 0:
+			vRandom = randint(0,(len(vowel)-1))
+			newWord += vowel[vRandom]
+		else:
+			cRandom = randint(0,(len(consonant)-1))
+			newWord += consonant[cRandom]
+			if consonant[cRandom] == "q":
+				newWord += vowel[4]
+
+	if "c" in newWord and "ie" in newWord:
+		print("detected spelling issue")
+		newWord1 = list(newWord)		
+		for i in range(len(newWord)):
+			if newWord[i] == "i" and newWord[i+1] == "e":
+				newWord1[i] = "e"
+				newWord1[i+1] = "i"
+				newWord = ''.join(newWord1)
 	
-	dicLength = len(dictionary)-1
-	word = "hobab"
+	if "gh" in newWord and "ie" in newWord:
+		print("detected spelling issue")
+		newWord1 = list(newWord)		
+		for i in range(len(newWord)):
+			if newWord[i] == "i" and newWord[i+1] == "e":
+				newWord1[i] = "e"
+				newWord1[i+1] = "i"
+				newWord = ''.join(newWord1)
+	
+	if newWord.endswith("able"):
+		newWord1 = newWord[:-4]
+		for j in range(dicLength):
+			if newWord1 == dictionary[j]:
+				ableLess = 1
+				
+		if ableLess == 0:
+			newWord = newWord1 + ible
+		ableLess = 0
+
+	if newWord.endswith("ible"):
+		newWord1 = newWord[:-4]
+		for j in range(dicLength):
+			if newWord1 == dictionary[j]:
+				ibleLess = 1
+				
+		if ibleLess == 1:
+			newWord = newWord1 + able
+		ibleLess = 0
+
+
+
+		
+
+
+	
+	
 	print newWord
 	for j in range(dicLength):
 		if newWord == dictionary[j]:
@@ -30,14 +89,15 @@ def buildIt(alphabet,dictionary):
 
 
 def readIn():
-	alphabet = []
+	vowels = []
+	consonants = []
 	dictionary = []
 
-	f = open("alphabet.txt","r")
+	f = open("vowels.txt","r")
 	for line in f:
 		lines = line.rstrip('\n')
 
-		alphabet.append(lines)
+		vowels.append(lines)
 		
 	g = open("dictionary.txt","r")
 	for line in g:
@@ -45,8 +105,16 @@ def readIn():
 
 		dictionary.append(lines)
 
+	h = open("consonants.txt","r")
+	for line in h:
+		lines = line.rstrip('\n')
 
-	buildIt(alphabet,dictionary)
+		consonants.append(lines)
+
+
+	buildIt(vowels,consonants,dictionary)
+
+
 
 
 
